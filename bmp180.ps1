040fc7b4ee4054c837e12900a7fbc97399cbc02e
garciaolais <garcia.olais@example.com>
2018-06-14 03:42:19 +0000

    Add files

---
+++ bmp180.ps1
@@ -0,0 +1,15 @@
+try
+{
+	Add-Type -Path "$(Resolve-Path .)/bmp180-netcore.dll"
+
+	$bmp180 =  [bmp180_netcore.BMP180]::new()
+
+	"$($bmp180.ReadTemperature() / 10)°C"
+	"$($bmp180.ReadPressure()) bar"
+	"$($bmp180.ReadAltitude().ToString("#.000")) mts"
+	"$($bmp180.ReadSeaAltitude()) pa"
+}
+catch
+{
+	Write-OutPut $_.Exception.Message
+}
